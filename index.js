cdd4c5ce0d87f23576dbdbfa897102c7e818029d
YeshwanthSivaraj <mail2yeshwanthsivaraj@gmail.com>
2021-08-19 20:37:53 +0530

    JSON parsing correction

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const got = require('got');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}))
 
 const port = process.env.PORT || 3000;
 
